0ecfa7564fba4f597856469fd9bfff0445484c21
Asim Craft <vectornpixel@gmail.com>
2016-06-18 02:45:30 -0400

    grunt

--- Website/wp-content/themes/btshiphop/Gruntfile.js
+++ Website/wp-content/themes/btshiphop/Gruntfile.js
@@ -1,121 +1,41 @@
 module.exports = function (grunt) {
   'use strict';
-  var packageInfo = require('./package'),
-      webHome = grunt.option('web'),
-      serverPort = grunt.option('port') || 8777,
-      proxy = require('proxy-middleware'),
-      url = require('url'),
-      ampProxyOpts = url.parse('https://api2.iheart.com/api');
-
-  ampProxyOpts.route = '/amp';
-
   grunt.initConfig({
     clean: {
       build: ['build']
-    },
-    'saucelabs-qunit': {
-      dev: {
-        options: {
-          //Set creds in ENV vars
-          // export SAUCE_USERNAME=XXX
-          // export SAUCE_ACCESS_KEY=XXX
-          urls: ['http://localhost:' + serverPort + '/tests/qunit/runners/basic.html'],
-          tunnelTimeout: 5,
-          build: packageInfo.version,
-          concurrency: 3,
-          'avoid-proxy': false,
-          browsers: [
-            {
-              browserName: 'chrome',
-              version: '34',
-              platform: 'Windows 7'
-            }
-          ],
-          testInterval: 60000,
-          testname: 'player qunit tests',
-          tags: ['master', 'dev', packageInfo.version]
-        }
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: serverPort,
-          hostname: '0.0.0.0',
-          middleware: function(connect, options, middlewares) {
-            // inject a custom middleware into the array of default middlewares
-            middlewares.push(proxy(ampProxyOpts));
-            return middlewares;
-          }
-        }
-      },
-      alive: {
-        options: {
-          port: serverPort,
-          keepalive: true,
-          hostname: '0.0.0.0',
-           middleware: function(connect, options, middlewares) {
-            // inject a custom middleware into the array of default middlewares
-            middlewares.push(proxy(ampProxyOpts));
-            return middlewares;
-          }
-        }
-      }
-    },
-    browserify: {
-      client: {
-        src: ['index.js'],
-        dest: 'dist/player.js',
-        options: {
-          // For weblibjs so that browserify doesn't try to resolve request
-          ignore: ['request'],
-          browserifyOptions: {
-            standalone: 'IHRPlayer',
-            debug: true
-          }
-        }
-      },
-      test: {
-        src: ['./tests/suite.js'],
-        dest: 'tests/suite.browserified.js',
-        options: {
-          // For weblibjs so that browserify doesn't try to resolve request
-          ignore: ['request'],
-          browserifyOptions: {
-            debug: true
-          }
-        }
-      }
     },
     jshint: {
       options: {
         jshintrc: true
       },
-      all: ['lib/**/*.js']
+      all: ['js/*.js']
     },
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['tests/mocha/**/test.*.js']
+    concat: {
+      dist: {
+        src: ['js/*.js'],
+        dest: 'js/scripts.js'
       }
     },
     watch: {
-      build: {
-        files: ['lib/**/*.js', 'tests/**/*.js'],
-        tasks: ['jshint', 'jsdoc', 'browserify', 'uglify']
+      sass: {
+        files: [
+          'assets/sass/main.scss',
+          'assets/sass/**/*.scss'
+        ],
+        tasks: ['sass:build']
       },
-      dev: {
-        files: ['lib/**/*.js', 'tests/**/*.js'],
-        tasks: ['test']
-      }
+      build: {
+        files: ['js/*.js'],
+        tasks: ['jshint', 'browserify', 'uglify']
+      },
     },
-    concurrent: {
-      target: {
-        tasks: ['server', 'watch:build'],
-        options: {
-          logConcurrentOutput: true
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'main.css': 'main.scss'
         }
       }
     },
@@ -125,33 +45,38 @@ module.exports = function (grunt) {
           sourceMap: true
         },
         files: {
-          'dist/player.min.js': ['dist/player.js']
+          'js/scripts.min.js': ['js/scripts.js']
         }
       }
     }
   });
 
-  grunt.registerTask('jsdoc', 'Generate jsdoc', function () {
-    var exec = require('child_process').exec
-      , done = this.async();
-    grunt.log.writeln('Generating jsdoc...');
-    exec('jsdoc README.md -c conf.json -r -t node_modules/ink-docstrap/template', done);
-  });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-saucelabs');
   grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-connect-proxy');
-  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('server', ['build', 'connect:alive']);
-  grunt.registerTask('build', ['jshint', 'browserify', 'uglify']);
-  grunt.registerTask('test', ['build', 'connect:server', 'configureProxies']);
-  grunt.registerTask('test-sauce', ['build', 'connect:server', 'saucelabs-qunit']);
-  grunt.registerTask('default', ['concurrent']);
+  // Register tasks
+  grunt.registerTask('default', [
+    'dev',
+    'build'
+  ]);
+  grunt.registerTask('dev', [
+    'jshint',
+    'concat'
+  ]);
+  grunt.registerTask('build', [
+    'sass',
+    'uglify'
+  ]);
+
+  grunt.task.run([
+    'open',
+    'watch'
+  ]);
 };
