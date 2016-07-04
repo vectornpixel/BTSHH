bd0e8faafb8d13b0df6670957a9fd084960ff437
Asim Craft <vectornpixel@gmail.com>
2016-07-03 22:39:05 -0400

    footer

--- Website/wp-content/themes/btshiphop/footer.php
+++ Website/wp-content/themes/btshiphop/footer.php
@@ -13,45 +13,35 @@
 
 	</div><!-- #content -->
 
-<footer id="colophon" class="site-footer" role="contentinfo">
+<div id="footer-sidebar" class="secondary site-footer">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4">
-				<h4 class="text-uppercase">About</h4>
-				<p>CMS was established in 1998. GBS is a minority partner of CMS –an
-					experienced, minority-owned and operated partner for innovative solutions. We focus on providing value-added solutions that are customized and flexible to each customer’s needs.</p>
+				<?php
+				if(is_active_sidebar('footer-sidebar-1')){
+					dynamic_sidebar('footer-sidebar-1');
+				}
+				?>
 			</div>
 			<div class="col-md-4">
-				<h4 class="text-uppercase">Location</h4>
-				<p>Corporate Headquarters</p>
-				<p>
-					7233 Freedom Avenue, NW<br>
-					North Canton, Ohio 44720<br>
-					330.498.1013</p>
+				<?php
+				if(is_active_sidebar('footer-sidebar-2')){
+					dynamic_sidebar('footer-sidebar-2');
+				}
+				?>
 			</div>
 			<div class="col-md-4">
-				<h4 class="text-uppercase">Contact Us</h4>
-				<?php echo do_shortcode( '[contact-form-7 id="208" title="Contact form 1"]' ); ?>
-				<!--<form class="form-container" name="contact-form" method="post">
-					<div class="form-group">
-						<input class="form-control form-field" type="text" name="name" id="name" placeholder="Name" />
-					</div>
-					<div class="form-group">
-						<input class="form-control form-field" type="text" name="email" id="email" placeholder="Email" />
-					</div>
-					<div class="form-group">
-						<input class="form-control form-field" type="text" name="phone" id="phone" placeholder="Phone" />
-					</div>
-
-					<div class="form-group pull-right">
-						<button class="btn btn-submit" type="submit">Send Email</button>
-					</div>
-				</form>-->
+				<?php
+				if(is_active_sidebar('footer-sidebar-3')){
+					dynamic_sidebar('footer-sidebar-3');
+				}
+				?>
 			</div>
 		</div>
+
 	</div>
 
-</footer>
+</div>
 
 </div><!-- #page -->
 
